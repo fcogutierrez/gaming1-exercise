8638f9b2d9b537fc1c9f1159871ab448d75d0742
fcogutierrez <fcojavier.gutierrezg@gmail.com>
2025-04-18 10:57:40 +0200

    Adding command handler for the guess mistery number

---
+++ HiLoGame/Application/CommandHandlers/GuessMisteryNumberCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.Commands;
+using Application.Contracts;
+using Domain.Factories;
+using Domain.Model;
+
+namespace Application.CommandHandlers;
+
+public sealed class GuessMisteryNumberCommandHandler(IEventStorage eventStorage)
+    : ICommandHandler<GuessMisteryNumberCommand, GuessMisteryNumberCommandResult>
+{
+    private readonly IEventStorage _eventStorage = eventStorage;
+
+    public async Task<GuessMisteryNumberCommandResult> Handle(GuessMisteryNumberCommand command)
+    {
+        var events = await _eventStorage.GetByAggregateId(command.GameId);
+        var game = AggregateFactory.Create<GameAggregate>(events);
+
+        game.GuessMisteryNumber(command.PlayerId, command.Guess);
+
+        return new GuessMisteryNumberCommandResult();
+    }
+}
---
+++ HiLoGame/Application/Commands/GuessMisteryNumberCommand.cs
@@ -0,0 +1,7 @@
+﻿using Application.Contracts;
+
+namespace Application.Commands;
+
+public sealed record GuessMisteryNumberCommand(Guid GameId, Guid PlayerId, int Guess) : ICommand;
+
+public sealed record GuessMisteryNumberCommandResult() : ICommandResult;
