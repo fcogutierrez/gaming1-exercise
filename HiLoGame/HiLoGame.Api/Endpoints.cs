d87f8e80c9bf13b6e2a599eaa75b06e22e1c3b72
Francisco J. Gutiérrez <fcojavier.gutierrezg@gmail.com>
2025-04-18 22:21:23 +0200

    Deleting the default endpoint

---
+++ HiLoGame/HiLoGame.Api/Endpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace HiLoGame.Api;
+
+internal static class Endpoints
+{
+    public static IEndpointRouteBuilder UseHiLoGameEndpoints(this IEndpointRouteBuilder app)
+    {
+        RouteGroupBuilder api = app.MapGroup("/games/hilo");
+
+        api.MapGet("/", () => Results.Ok("Welcome to the HiLo Game API!"));
+
+        return app;
+    }
+}
